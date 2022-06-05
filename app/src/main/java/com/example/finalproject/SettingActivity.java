6eb3972a8119ec712d7bd34a476948bd4b2eed7e
TranBinhLuatUIT <76168991+TranBinhLuatUIT@users.noreply.github.com>
2022-06-05 14:02:32 +0700

    add setting activity and seekbar

--- app/src/main/java/com/example/finalproject/SettingActivity.java
+++ app/src/main/java/com/example/finalproject/SettingActivity.java
@@ -1,14 +1,56 @@
 package com.example.finalproject;
 
 import android.os.Bundle;
+import android.widget.SeekBar;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class SettingActivity extends AppCompatActivity {
-
+    SeekBar seekBarDis, seekBarAge;
+    TextView textViewDis, textViewAge;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_setting);
+
+        seekBarDis = findViewById(R.id.SeekBarID);
+        textViewDis = findViewById(R.id.distanceID);
+        seekBarDis.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
+                textViewDis.setText(String.valueOf(i)+"km.");
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+
+        seekBarAge = findViewById(R.id.SeekBarAgeID);
+        textViewAge = findViewById(R.id.ageID);
+        seekBarAge.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
+                textViewAge.setText("18 - "+ String.valueOf(i));
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+
     }
 }
