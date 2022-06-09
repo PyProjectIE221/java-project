0fcbcde7d42566ecc5db6d9f967100cee5faad4c
TranBinhLuatUIT <76168991+TranBinhLuatUIT@users.noreply.github.com>
2022-06-09 21:51:36 +0700

    add feature forgot password

--- app/src/main/java/com/example/finalproject/LoginActivity.java
+++ app/src/main/java/com/example/finalproject/LoginActivity.java
@@ -4,8 +4,10 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.firebase.auth.FirebaseAuth;
@@ -15,6 +17,7 @@ import com.google.firebase.auth.FirebaseUser;
 
 public class LoginActivity extends AppCompatActivity {
     private EditText mEmail, mPassword;
+    private TextView forgotPassword;
     private FirebaseAuth mAuth;
     private FirebaseAuth.AuthStateListener authStateListener;
     @Override
@@ -57,6 +60,14 @@ public class LoginActivity extends AppCompatActivity {
                    }
                 }
             });
+        });
+        forgotPassword = findViewById(R.id.forgot);
+        forgotPassword.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(LoginActivity.this,ResetPasswordActivity.class));
+                finish();
+            }
         });
     }
 
